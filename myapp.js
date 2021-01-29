37675fe3fda28d320b5efc7f6ff27aef84769a2f
daveumeh <david.umeh93@gmail.com>
2021-01-29 12:13:39 +0100

    update

--- myapp.js
+++ myapp.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) => {
 res.send( {
-"message": "API response message",
+"message": "My Rule-validation API",
 "status": "success",
 "data": {
   "name": "Umeh Obioma David",
@@ -13,6 +13,22 @@ res.send( {
 "mobile": "2348136373911",
 "twitter": "@dave_da_CEO"  } } )
 })
+app.post('/validate-rule', function (req, res))
+res.send( {
+  "rule": {
+    "field": "missions",
+    "condition": "gte",
+    "condition_value": 30
+  },
+  "data": {
+    "name": "James Holden",
+    "crew": "Rocinante",
+    "age": 34,
+    "position": "Captain",
+    "missions": 45
+  }
+}
+)
 
 
 app.listen(port, () => {
