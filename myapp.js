806844a0fbe776be54580a066eb17e1eff2a1f20
daveumeh <david.umeh93@gmail.com>
2021-01-29 11:30:48 +0100

    Add files via upload

---
+++ myapp.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+app.get('/', (req, res) => {
+res.send( {
+"message": "API response message",
+"status": "success",
+"data": {
+  "name": "Umeh Obioma David",
+"github": "@daveumeh",
+"email": "david.umeh93@gmail.com",
+"mobile": "2348136373911",
+"twitter": "@dave_da_CEO"  } } )
+})
+git push
+
+app.listen(port, () => {
+console.log( 'listening at http://localhost:${port}')
+})
