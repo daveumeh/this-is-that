3ff47041df73aba1a5e09158f7efdd1fc55b07f9
daveumeh <david.umeh93@gmail.com>
2021-01-30 05:24:38 +0100

    complete

--- myapp.js
+++ myapp.js
@@ -62,56 +62,52 @@ function validator({ condition, field, conditionValue }) {
 }
 
 
-// your validate-rule route handler
 app.post('/validate-rule', function(req, res, next) {
   const { rule, data } = req.body || {};
 
-  // making sure rule is defined and present in request body
+
   if(!rule) {
     return res.status(400).json(errorMessage('rule is required'));
   }
 
-  // rule must be an object
-  // and not array since type of array returns object
+
   if (!(typeof rule === 'object' && !Array.isArray(rule))) {
     return res.status(400).json(errorMessage('rule should be an object.'));
   }
 
-  // making sure rule is defined and present in request body
+
   if(!data) {
     res.status(400).json(errorMessage('data is required'));
   }
 
-  // destructuring the rule object for validation
+
   const { field, condition, condition_value } = rule;
 
-  // making sure rule.field is defined and present
+
   if(!field) {
       return res.status(400).json(errorMessage('rule must have a \'field\' field.'));
   }
 
-  // making sure rule.condition is defined and present
+
   if (!condition) {
     return res.status(400).json(errorMessage('rule must have a \'condition\' field.'));
   }
 
-  // an array showing allowed conditions
+
   const validConditions = ['eq', 'neq', 'gte', 'gt', 'contains'];
 
-  // making sure condition gotten from user matched the ones allowed
+
   if(!validConditions.includes(condition)) {
     return res.status(400).json(errorMessage(`invalid rule condition passed.`);
   }
 
 
-  // condition_value has to be present in the rule field
+
   if (!condition_value) {
     return res.status(400).json(errorMessage('rule must have a \'condition_value\' field.'));
   }
 
-  // if data is a string or array
-  // and field is not equal to zero
-  // then the field does not exist
+
   if(
     (typeof data === 'string' || Array.isArray(data))
     && field !== '0'
@@ -119,16 +115,12 @@ app.post('/validate-rule', function(req, res, next) {
     return res.status(400).json(errorMessage(`field ${field} is missing from data.`));
   }
 
-  // if data is an object
-  // and field is not a key present in that object
-  // return error message
+
   if(typeof data === 'object' && !data[field]) {
       return res.status(400).json(errorMessage(`field ${field} is missing from data.`));
   }
 
-  // extract the field value
-  // if it is array or string return whole data
-  // if it is objet return the key corresponding to field in data
+
   const fieldValue = (Array.isArray(data) || typeof data === 'string') ? data : data[field];;
 
 
